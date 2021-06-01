fb2e7ae377003aae48788831b9ecdb7c5d595f07
kuratov-v <kuratovv29@gmail.com>
2021-06-01 18:35:04 +0300

    add token verify

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import token_verify
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/login/", include("rest_social_auth.urls_jwt_pair")),
+    path("api/v1/login/", include("rest_social_auth.urls_jwt_pair")),
+    path("api/v1/token-verify/", token_verify),
     path("api/v1/", include("src.budget.urls")),
     path("api/v1/", include("src.account.urls")),
     path("api/v1/", include("src.habit_tracker.urls")),
